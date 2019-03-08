92ab02a6f82cd8ddb826b89b10424a3007356fcd
ccatalonec <43669251+ccatalonec@users.noreply.github.com>
2019-03-08 12:56:38 +0200

    s

---
+++ js/app.js
@@ -0,0 +1,39 @@
+/* https://greensock.com/gsap */
+TweenLite.set("#petals", { perspective:600 })
+TweenLite.set("img", { xPercent:"-50%", yPercent:"-50%" })
+
+var total = 50;
+var warp = document.getElementById("petals"),
+    w = window.innerWidth,
+    h = window.innerHeight;
+
+for (i = 0; i < total; i++) {
+    var Div = document.createElement('div');
+    TweenLite.set(Div, { attr: { class:'dot' }, x:R(0,w), y:R(-200,-150), z:R(-200,200)});
+    warp.appendChild(Div);
+    animm(Div);
+}
+
+function animm(elm) {
+    TweenMax.to(elm,R(6,15), { y:h+100, ease:Linear.easeNone, repeat:-1, delay:-15 });
+    TweenMax.to(elm,R(4,8), { x:'+=100', rotationZ:R(0,180), repeat:-1, yoyo:true, ease:Sine.easeInOut });
+    TweenMax.to(elm,R(2,8), { rotationX:R(0,360), rotationY:R(0,360), repeat:-1, yoyo:true, ease:Sine.easeInOut, delay:-5 });
+};
+
+function R(min,max) { return min+Math.random() * (max-min) };
+
+
+/* https://mattboldt.com/typed.js/ */
+var typed = new Typed('#text', {
+    strings: ['Catalina, fie ca aceasta zi sa iti umple sufletul de fericire, emotie si de parfumul tuturor florilor. Sa ramai mereu asa cum te-am cunoscut, cu zambetul pe buze, plina de viata si mai frumoasa decat toate florile din lume. Spiritul acestei zile sa te insoteasca pretutindeni si sa ai doar impliniri si realizari frumoase. Sa stii ca iti sunt alaturi, te imbratisez cu drag si te pup!'],
+    startDelay: 1500,
+    typeSpeed: 15,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: false,
+    showCursor: false,
+    onComplete: function() {
+        var author = document.getElementById("author");
+        author.style.opacity = 1;
+    }
+});
